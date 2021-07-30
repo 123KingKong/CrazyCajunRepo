fb2ee87a3666c4153252a49fd4396140faa1338f
123KingKong <88099318+123KingKong@users.noreply.github.com>
2021-07-30 15:50:13 -0500

    Update addonvar.py

--- repo/plugin.program.simple19wizard/addonvar.py
+++ repo/plugin.program.simple19wizard/addonvar.py
@@ -5,7 +5,7 @@ from datetime import datetime
 addon_id = xbmcaddon.Addon().getAddonInfo('id')
 
 '''#####-----Build File-----#####'''
-buildfile = 'https://github.com/123KingKong/Build/blob/main/builds.xml'
+buildfile = 'https://raw.githubusercontent.com/123KingKong/Build/main/builds.xml'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'http://slamiousproject.com/wzrd/notify19.txt'
